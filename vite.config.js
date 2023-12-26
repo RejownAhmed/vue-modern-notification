22fe3861b6dccf49aacec7e82318cb8b841d3e96
“rejownahmed” <rejownahmedzisan@gmail.com>
2023-12-27 00:36:12 +0600

    vite-plugin-css-injected-by-js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import path from 'path';
 import { resolve } from 'path';
 
@@ -7,6 +8,7 @@ import { resolve } from 'path';
 export default defineConfig({
   plugins: [
     vue(),
+    cssInjectedByJsPlugin(),
   ],
   resolve: {
     alias: {
