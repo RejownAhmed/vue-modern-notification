2999e7d8fe49ceeac004b4cb967ce11ac399f240
“rejownahmed” <rejownahmedzisan@gmail.com>
2023-12-28 01:21:49 +0600

    Types update

--- src/plugins/notification/types.d.ts
+++ src/plugins/notification/types.d.ts
@@ -1,20 +1,34 @@
+export type color = "primary" | "secondary" | "success" | "warning" | "danger" | "light" | "dark" | "info";
+
 export interface Colors {
-  primary?: number[]
-  secondary?: number[]
-  success?: number[]
-  warning?: number[]
-  danger?: number[]
+  primary?: [number, number, number]
+  secondary?: [number, number, number]
+  success?: [number, number, number]
+  warning?: [number, number, number]
+  danger?: [number, number, number]
 }
 
 export interface Configuration {
   colors?: Colors
+  position?: string
+  border?: {
+    borderWidth?: string
+    color?: color
+  }
+  duration?: boolean|number
+  flat?: boolean
+  sticky?: boolean
+  square?: boolean
+  width?: string
+  noPadding?: boolean
+  showProgress?: boolean
 }
 
 export interface NotificationParams {
   title?: string
   text?: string
   position?: string
-  color?: string
+  color?: color
   border?: string
   icon?: string
   duration?: number | boolean
@@ -27,7 +41,7 @@ export interface NotificationParams {
   square?: boolean
   width?: string
   loading?: boolean
-  noPadding?: any
+  noPadding?: boolean
   customComponent?: any
   closeOnClick?: boolean
   classNotification?: string
